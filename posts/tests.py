cf99656e8e761ae86471283e217e1be8c32e37a6
dimah <dimah.code@gmail.com>
2025-12-15 11:36:20 +0330

    tests: add tests to posts/tests.py

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,27 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Post
+
+class BlogTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create(
+            username="testuser",
+            email="test@gmail.com",
+            password="secrete123",
+        )
+        cls.post = Post.objects.create(
+            title='Post Title',
+            body='Post Body',
+            author=cls.user,
+        )
+
+    def test_post_model(self):
+        self.assertEqual(self.post.title, 'Post Title')
+        self.assertEqual(self.post.body, 'Post Body')
+        self.assertEqual(str(self.post), 'Post Title')
+        self.assertEqual(self.post.author.username, "testuser")
+
+

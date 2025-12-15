49a61ec76c231f20b4340d67326e86b0c5c07ce8
dimah <dimah.code@gmail.com>
2025-12-15 12:41:32 +0330

    fix: import posts/views.py to posts/urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import render
+from .views import PostDetail, PostList
 
 urlpatterns = [
     path("<int:pk>/", PostDetail.as_view(), name="post-detail"),

2509ac457123254157a68b0236be96200940d69b
dimah <dimah.code@gmail.com>
2025-12-17 14:27:06 +0330

    feat: add schema path to config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_spectacular.views import SpectacularAPIView
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
+    # Blog
     path('api/v1/', include('posts.urls')),
+    # Auth
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/', include('dj_rest_auth.urls')),
     path('api/v1/register/', include('dj_rest_auth.registration.urls')),
+    # Schema
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
 ]
